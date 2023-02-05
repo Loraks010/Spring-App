baec523024b017aebdea3a584f7d7b6f5f43242a
Vladimir <idk>
2023-02-05 22:00:45 +0300

    searching products added

--- src/main/java/com/app/flower_shop/controllers/HomeController.java
+++ src/main/java/com/app/flower_shop/controllers/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -15,8 +20,17 @@ public class HomeController {
 
 
     @GetMapping("/assortment")
-    public String assortmentGet( Model model) {
-        Iterable<Product> products=productRepository.findAll();
+    public String assortmentGet(Model model) {
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "flower_shop-assortment";
+    }
+
+    @PostMapping("filter")
+    public String search(@RequestParam String text, Model model) {
+        List<Product> products = productRepository.findByDescriptionContaining(text);
+        products.addAll(productRepository.findByNameContaining(text));
+        products=products.stream().distinct().collect(Collectors.toList());
         model.addAttribute("products",products);
         return "flower_shop-assortment";
     }
--- src/main/java/com/app/flower_shop/repos/ProductRepository.java
+++ src/main/java/com/app/flower_shop/repos/ProductRepository.java
@@ -3,5 +3,10 @@ package com.app.flower_shop.repos;
 import com.app.flower_shop.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product,Long> {
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByDescriptionContaining(String text);
+
+    List<Product> findByNameContaining(String text);
 }
